cbda621d1a67026ccb5a71713a4168bf65ff1dae
Lucas Lion <101745765+Lucas-Lion@users.noreply.github.com>
2023-01-18 16:48:55 -0300

    estudos

--- estudos js/Aula 11/ex008/ex008.js
+++ estudos js/Aula 11/ex008/ex008.js
@@ -1,1 +1,4 @@
-console.log('Testando console!')
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+console.log(`Você ultrapassou o limite de velocidade. MULTADO!`)
+console.log(`Dirija com cuidado!`)
