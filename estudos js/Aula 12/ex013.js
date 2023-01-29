b696c8ee2bd8b7542b09e127361b8a6f1a70ade4
Lucas-Lion <lucaslion350@gmail.com>
2023-01-28 21:47:21 -0300

    estudos

--- estudos js/Aula 12/ex013.js
+++ estudos js/Aula 12/ex013.js
@@ -1,9 +1,10 @@
-var hora = 8
+var agora = new Date()
+var hora = agora.getHours()
 console.log(`agora são exatamente ${hora} horas.`)
 if (hora < 12) {
     console.log('Bom dia!')
-} else if (hora < 17 && hora > 18) {
-    console.log('Boa tarde')
+} else if (hora <= 18) {
+    console.log('Boa tarde!')
 } else {
-    console.log('Boa noite')
+    console.log('Boa noite!')
 }
