fa0b004e7fb0d0fc1d41282af9c504ce6f77b074
Lucas Lion <lucaslion350@gmail.com>
2023-02-16 10:59:39 -0300

    atualizando ex 01

--- estudos js/exercicio pt 1/ex015/script.js
+++ estudos js/exercicio pt 1/ex015/script.js
@@ -1,1 +1,22 @@
-alert('Olá')
+function carregar() {
+    const msg = document.getElementById('msg')
+    let img = document.getElementById('imagem')
+    const data = new Date()
+    const hora = data.getHours()
+    
+    msg.innerHTML = `<strong>Agora são ${hora} horas.</strong>`
+
+    if (hora >= 0 && hora < 12) {
+        //GOOD MORNING 
+        img.src = 'img/morning.png'
+        document.body.style.background = '#967a65'
+    } else if (hora >= 12 && hora <= 18) {
+        //GOOD AFTERNOON 
+        img.src = 'img/afternoon.png'
+        document.body.style.background = '#330a04'
+    } else {
+        //GOOD NIGHT 
+        img.src = 'img/night.png'
+        document.body.style.background = '#0e141f'
+    }
+} 
