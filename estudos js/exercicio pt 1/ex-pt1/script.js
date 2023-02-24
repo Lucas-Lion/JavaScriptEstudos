59c4ec993aef64ec386fd9cc657f8e6cf67620d1
Lucas-Lion <lucaslion350@gmail.com>
2023-02-24 04:12:16 -0300

    estudos js

---
+++ estudos js/exercicio pt 1/ex-pt1/script.js
@@ -0,0 +1,12 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('ano')
+    var res = document.getElementById('res')
+    
+    if (fano.ariaValueMax.length == 0 || fano.value > ano) {
+        window.alert('[ERRO] Verifique e tente novamente!')
+    } else {
+        window.alert('tudo certo')
+    }
+}
