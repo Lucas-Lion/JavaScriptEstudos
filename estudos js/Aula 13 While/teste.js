3b42f81b95dc9402e5741403dc6b36012ada22af
Lucas-Lion <lucaslion350@gmail.com>
2023-03-18 23:16:20 -0300

    while e for

--- estudos js/Aula 13 While/teste.js
+++ estudos js/Aula 13 While/teste.js
@@ -1,6 +1,6 @@
 var c = 1
 
-while (c <= 6) {
-    console.log(`Passo ${c}`)
-    c++ // c = c + 1
+while (c <= 10) {
+    console.log (`Bazinga!`)
+    c++
 }
---
+++ estudos js/Aula 14 For/teste.js
@@ -0,0 +1,5 @@
+console.log('Vai iniciar...')
+for (var c=1; c<=10; c++) {
+    console.log('bazinga!')
+}
+console.log('The End!')
