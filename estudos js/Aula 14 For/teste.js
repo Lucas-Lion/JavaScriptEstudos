c79cf2a2eb2975f998796d309dc1891b8427ce11
Lucas-Lion <lucaslion350@gmail.com>
2023-03-19 00:05:01 -0300

    estudos JS

--- estudos js/Aula 14 For/teste.js
+++ estudos js/Aula 14 For/teste.js
@@ -1,5 +1,13 @@
 console.log('Vai iniciar...')
-for (var c=1; c<=10; c++) {
+for (var c=1; c<=9; c++) {
     console.log('bazinga!')
 }
 console.log('The End!')
+
+
+/* Estrutura do FOR */
+
+// Variavel 1
+// while 2
+// acrescimo 3
+// ação 4 { }
